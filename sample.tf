21ffb303b8b6496d52e8f3ca53eb8e0700a1c796
sunil <sunilkumar.ipm@gmail.com>
2023-05-08 17:10:16 +0530

    terraform

--- sample.tf
+++ sample.tf
@@ -1,8 +1,24 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
+  }
+}
+
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
   }
 }
