620c5272d2b3ed8a366036c286e73d2baa912e3d
sunil <sunilkumar.ipm@gmail.com>
2023-05-09 12:36:05 +0530

    terraform

---
+++ 01.Vars/outputs.tf
@@ -0,0 +1,7 @@
+output "sample_list" {
+value=var.sample_list[1]
+}
+
+output "sample_list" {
+value=var.sample_list[3]
+}
---
+++ 01.Vars/vars.tf
@@ -0,0 +1,9 @@
+# We have list variable type , Meaning single variable can hold multiple values
+variable "sample_list" {
+default=[
+100,
+Number1,
+"hello",
+boolean
+ ]
+}
