14d84f1a9a476076fa507dddb903640d3c3cd8e5
sunil <sunilkumar.ipm@gmail.com>
2023-05-09 12:54:59 +0530

    terraform

--- 01.Vars/outputs.tf
+++ 01.Vars/outputs.tf
@@ -1,7 +1,6 @@
 output "sample_list_1" {
 value = var.sample_list01[1]
 }
-
-##output "sample_list_2" {
-  ##value = var.sample_list[1]
-##}
+output "sample_list_2" {
+  value = var.sample_list[1]
+}
