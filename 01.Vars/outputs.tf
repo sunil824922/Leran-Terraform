7b18eedb64a413cd23789e1d48adde82784e757f
sunil <sunilkumar.ipm@gmail.com>
2023-05-09 12:39:57 +0530

    terraform

--- 01.Vars/outputs.tf
+++ 01.Vars/outputs.tf
@@ -1,7 +1,7 @@
 output "sample_list1" {
-value=var.sample_list[1]
+value=var.sample_list01[1]
 }
 
 output "sample_list2" {
-value=var.sample_list[3]
+value=var.sample_list01[3]
 }
--- 01.Vars/vars.tf
+++ 01.Vars/vars.tf
@@ -1,5 +1,5 @@
 # We have list variable type , Meaning single variable can hold multiple values
-variable "sample_list" {
+variable "sample_list01" {
 default=[
 100,
 Number1,
