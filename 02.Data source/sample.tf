d198261db99d2fd327336ff770eb7da74a5bbdd0
sunil <sunilkumar.ipm@gmail.com>
2023-05-10 17:44:23 +0530

    terraform

--- 02.Data source/sample.tf
+++ 02.Data source/sample.tf
@@ -9,10 +9,14 @@ data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
 
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
@@ -21,27 +25,27 @@ resource "aws_instance" "frontend" {
 }
 
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z07723241NASCD733XBTT"
-  name    = "frontend-dev.devops2023sk.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-  tags = {
-    Name = "mongodb"
-  }
-}
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z07723241NASCD733XBTT"
-  name    = "mongodb-dev.devops2023sk.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z07723241NASCD733XBTT"
+#  name    = "frontend-dev.devops2023sk.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
+#
+#
+#resource "aws_instance" "mongodb" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+#  tags = {
+#    Name = "mongodb"
+#  }
+#}
+#resource "aws_route53_record" "mongodb" {
+#  zone_id = "Z07723241NASCD733XBTT"
+#  name    = "mongodb-dev.devops2023sk.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.mongodb.private_ip]
+#}
