bf60498f915c87f5b4bb25706b822a9b4ad3426c
sunil <sunilkumar.ipm@gmail.com>
2023-05-10 13:17:01 +0530

    terraform

---
+++ 02.Data source/sample.tf
@@ -0,0 +1,51 @@
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instance_type"  {
+  default= "t3.micro"
+}
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "var.instance_type"
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z07723241NASCD733XBTT"
+  name    = "frontend-dev.devops2023sk.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = "var.instance_type"
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z07723241NASCD733XBTT"
+  name    = "mongodb-dev.devops2023sk.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
